3ba668f0ad2c0dc2e7aaf15f772c7d479989ca5f
Martin Stewart <martin.stewart@insurello.se>
2021-10-05 13:11:31 +0200

    Preparing for v6

---
+++ src/Evergreen/V6/Cache.elm
@@ -0,0 +1,25 @@
+module Evergreen.V6.Cache exposing (..)
+
+import AssocList
+import Dict
+import Elm.Syntax.Range
+import Evergreen.V6.TranslationParser
+import List.Nonempty
+
+
+type alias CachedTranslation =
+    { functionName : String
+    , translations :
+        AssocList.Dict
+            (List.Nonempty.Nonempty String)
+            (Result
+                ()
+                { value : List.Nonempty.Nonempty Evergreen.V6.TranslationParser.Content
+                , range : Elm.Syntax.Range.Range
+                }
+            )
+    }
+
+
+type alias Cache =
+    Dict.Dict Int (List CachedTranslation)
---
+++ src/Evergreen/V6/TranslationParser.elm
@@ -0,0 +1,27 @@
+module Evergreen.V6.TranslationParser exposing (..)
+
+import AssocList
+import Elm.Syntax.Expression
+import Elm.Syntax.Range
+import List.Nonempty
+
+
+type Content
+    = TextContent String
+    | Placeholder Elm.Syntax.Expression.Expression
+
+
+type alias TranslationDeclaration =
+    { filePath : String
+    , functionName : String
+    , language : String
+    , translations :
+        AssocList.Dict
+            (List.Nonempty.Nonempty String)
+            (Result
+                ()
+                { value : List.Nonempty.Nonempty Content
+                , range : Elm.Syntax.Range.Range
+                }
+            )
+    }
