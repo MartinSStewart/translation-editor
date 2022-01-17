bdab07658751f515a67efbdc3ecb1eb7c8079c4a
Martin Stewart <martin.stewart@insurello.se>
2022-01-17 13:47:10 +0100

    Preparing for v10

---
+++ src/Evergreen/V10/Cache.elm
@@ -0,0 +1,16 @@
+module Evergreen.V10.Cache exposing (..)
+
+import AssocList
+import Dict
+import Evergreen.V10.TranslationParser
+import List.Nonempty
+
+
+type alias CachedTranslation =
+    { functionName : String
+    , translations : AssocList.Dict (List.Nonempty.Nonempty String) (Result () Evergreen.V10.TranslationParser.TranslationValue_)
+    }
+
+
+type alias Cache =
+    Dict.Dict Int (List CachedTranslation)
---
+++ src/Evergreen/V10/Types.elm
@@ -0,0 +1,195 @@
+module Evergreen.V10.Types exposing (..)
+
+import AssocList
+import Browser
+import Browser.Navigation
+import Bytes
+import Evergreen.V10.Cache
+import Evergreen.V10.Github
+import Evergreen.V10.TranslationParser
+import Http
+import Lamdera
+import List.Nonempty
+import Set
+import Url
+
+
+type alias StartModel =
+    { personalAccessToken : String
+    , pressedSubmit : Bool
+    , loginFailed : Bool
+    , cache : Maybe Evergreen.V10.Cache.Cache
+    , branch : Maybe Evergreen.V10.Github.Branch
+    }
+
+
+type alias LoadingModel =
+    { oauthToken : Evergreen.V10.Github.OAuthToken
+    , filesRemaining : List ( String, Url.Url )
+    , directoriesRemaining : Set.Set String
+    , fileContents : List ( String, String )
+    , cache : Maybe Evergreen.V10.Cache.Cache
+    , branch : Maybe Evergreen.V10.Github.Branch
+    }
+
+
+type alias TranslationId =
+    { filePath : String
+    , functionName : String
+    , path : List.Nonempty.Nonempty String
+    }
+
+
+type alias ParsingModel =
+    { unparsedFiles : List ( String, String )
+    , parsedFiles :
+        List
+            { path : String
+            , result : List Evergreen.V10.TranslationParser.TranslationDeclaration
+            , original : String
+            }
+    , oauthToken : Evergreen.V10.Github.OAuthToken
+    , loadedChanges : AssocList.Dict TranslationId String
+    , cache : Maybe Evergreen.V10.Cache.Cache
+    , branch : Evergreen.V10.Github.Branch
+    }
+
+
+type SubmitStatus
+    = NotSubmitted
+        { pressedSubmit : Bool
+        }
+    | SubmitConfirm
+        (List.Nonempty.Nonempty
+            { path : String
+            , content : String
+            }
+        )
+    | Submitting
+    | SubmitSuccessful
+        { apiUrl : String
+        , htmlUrl : String
+        }
+    | SubmitFailed
+        { pressedSubmit : Bool
+        , error : ( String, Http.Error )
+        }
+
+
+type IsMarkdown
+    = IsMarkdown
+    | IsPartiallyMarkdown
+    | IsPlainText
+
+
+type alias TranslationGroup =
+    { path : List.Nonempty.Nonempty String
+    , filePath : String
+    , functionNames : List.Nonempty.Nonempty String
+    , isMarkdown : IsMarkdown
+    }
+
+
+type alias EditorModel =
+    { files :
+        AssocList.Dict
+            String
+            { original : String
+            }
+    , translations : List Evergreen.V10.TranslationParser.TranslationDeclaration
+    , oauthToken : Evergreen.V10.Github.OAuthToken
+    , changes : AssocList.Dict TranslationId String
+    , submitStatus : SubmitStatus
+    , pullRequestMessage : String
+    , hiddenLanguages : Set.Set String
+    , changeCounter : Int
+    , showOnlyMissingTranslations : Bool
+    , name : String
+    , branch : Evergreen.V10.Github.Branch
+    , allLanguages : Set.Set String
+    , groups : List TranslationGroup
+    }
+
+
+type State
+    = Start StartModel
+    | Authenticate (Maybe Evergreen.V10.Cache.Cache) (Maybe Evergreen.V10.Github.Branch)
+    | Loading LoadingModel
+    | Parsing ParsingModel
+    | Editor EditorModel
+    | ParsingFailed
+        { path : String
+        , branch : Evergreen.V10.Github.Branch
+        }
+    | LoadFailed Http.Error
+
+
+type alias FrontendModel =
+    { windowWidth : Int
+    , windowHeight : Int
+    , navKey : Browser.Navigation.Key
+    , state : State
+    , dummyChange : ()
+    }
+
+
+type alias BackendModel =
+    {}
+
+
+type FrontendMsg
+    = PressedLink Browser.UrlRequest
+    | UrlChanged
+    | TypedPersonalAccessToken String
+    | PressedSubmitPersonalAccessToken
+    | GotLocalStorageData
+        (Result
+            String
+            { key : String
+            , value : Maybe String
+            }
+        )
+    | ParsedFile
+        { path : String
+        , result : Result () (List Evergreen.V10.TranslationParser.TranslationDeclaration)
+        , original : String
+        }
+    | TypedTranslation TranslationId String
+    | GotWindowSize Int Int
+    | PressedSubmitChanges
+    | PressedConfirmSubmitChanges
+    | PressedCancelSubmitChanges
+    | PullRequestCreated
+        (Result
+            ( String, Http.Error )
+            { apiUrl : String
+            , htmlUrl : String
+            }
+        )
+    | PressedShowLanguage String
+    | PressedHideLanguage String
+    | TypedPullRequestMessage String
+    | TypedName String
+    | DebounceFinished
+        { changeCounter : Int
+        }
+    | PressedResetTranslationGroup
+        { path : List.Nonempty.Nonempty String
+        }
+    | PressedCloseSubmitSuccessful
+    | PressedToggleOnlyMissingTranslations
+
+
+type ToBackend
+    = AuthenticateRequest Evergreen.V10.Github.OAuthCode
+    | GetZipRequest Evergreen.V10.Github.OAuthToken (Maybe Evergreen.V10.Github.Branch)
+
+
+type BackendMsg
+    = GotAccessToken Lamdera.ClientId (Result Http.Error Evergreen.V10.Github.AccessTokenResponse)
+    | LoadedZipBackend Lamdera.ClientId (Result Http.Error ( Evergreen.V10.Github.Branch, Bytes.Bytes ))
+
+
+type ToFrontend
+    = AuthenticateResponse (Result Http.Error Evergreen.V10.Github.OAuthToken)
+    | GetZipResponse (Result Http.Error ( Evergreen.V10.Github.Branch, Bytes.Bytes ))
